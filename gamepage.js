32d13e405903566453926f4d5af76c2eb5b1148d
Wombbus <107441667+Wombbus@users.noreply.github.com>
2022-07-14 16:47:20 -0400

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,19 @@
+// random value generated
+  var x = document.getElementById("guessField").value
+
+  if(x == y){
+    alert("CONGRATS!!!"+PLAYERNAME+"YOU GUESSED IT RIGHT"+guess+"GUESS")
+    guess=1
+  }
+  else if(x>y){
+    guess++
+    alert("OOPS SORRY!! TRY A SMALLER NUMBER")
+  }
+  else{
+    guess++
+    alert("OOPS sORRY!! TRY A GREATER NUMBER")
+  }
+
+  function playAgain(){
+    y = Math.floor(Math.random() * 10+1)
+  }
